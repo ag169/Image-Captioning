b07388a9bc9a7ddd83ed450b9942dc86d5f4cbf9
Anshul Gupta <megaanshulgupta96@gmail.com>
2021-11-11 10:27:19 -0500

    Add compute_bleu_cider

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -67,6 +67,56 @@ class COCOEvalCap:
 
         return eval
 
+    def compute_bleu_cider(self):
+        imgIds = self.params['image_id']
+        # imgIds = self.coco.getImgIds()
+        gts = {}
+        res = {}
+        for imgId in imgIds:
+            gts[imgId] = self.coco.imgToAnns[imgId]
+            res[imgId] = self.cocoRes.imgToAnns[imgId]
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('tokenization...')
+        tokenizer = PTBTokenizer()
+        gts = tokenizer.tokenize(gts)
+        res = tokenizer.tokenize(res)
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('setting up scorers...')
+        scorers = [
+            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+            (Cider(), "CIDEr")
+        ]
+
+        # =================================================
+        # Compute scores
+        # =================================================
+        eval = {}
+
+        for scorer, method in scorers:
+            print('computing %s score...' % (scorer.method()))
+            score, scores = scorer.compute_score(gts, res)
+            if type(method) == list:
+                for sc, scs, m in zip(score, scores, method):
+                    self.setEval(sc, m)
+                    self.setImgToEvalImgs(scs, imgIds, m)
+                    print("%s: %0.3f" % (m, sc))
+                    eval[m] = sc
+            else:
+                self.setEval(score, method)
+                self.setImgToEvalImgs(scores, imgIds, method)
+                print("%s: %0.3f" % (method, score))
+                eval[method] = score
+
+        self.setEvalImgs()
+
+        return eval
+
     def setEval(self, score, method):
         self.eval[method] = score
 
