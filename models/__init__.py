5c7d4d2a8a62a8744b52b02d2d6a8c735259a982
Anshul Gupta <agupta845@gatech.edu>
2021-12-04 02:05:41 -0500

    Fix typo

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from .resnext50_lstm import ResNeXT50LSTM
 from .resnext50_gru import ResNeXT50GRU
 from .resnet50_lstm_attention import ResNet50LSTMAttention
 from .resnet50_lstm_attention_v2 import ResNet50LSTMAttention as ResNet50LSTMAttention2
-from .resnetx50_gru_attention_v2 import ResNeXT50GRUAttention
+from .resnext_50_gru_attention_v2 import ResNeXT50GRUAttention
 
 models = {
     'r50_lstm': ResNet50LSTM,
